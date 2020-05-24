4dc064012f694e002fc76c3038a8d40caaabb7a2
玉印 杨 <504097978@qq.com>
2020-05-24 21:27:20 +0800

    03 Scetion,Pass Test 8

--- BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,12 +1,142 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HttpModule.h"
+#include "IHttpResponse.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    DownloadWords();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
+    if (bGameOver)
+    {
+        ClearScreen();
+        SetupGame();
+        return;
+    }
+    
+    ProcessGame(Input);
+}
 
+void UBullCowCartridge::SetupGame()
+{
+    PrintLine(TEXT("Valid Words : %i"),HiddenWords.Num());
+    
+    HiddenWord = HiddenWords[0];
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+
+    //Welcome The Player
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the %i letter word!"),HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives."),Lives);
+    PrintLine(TEXT("Press <Enter> to continue..."));
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    
+    PrintLine(TEXT("Press <Enter> to play again..."));
+}
+
+void UBullCowCartridge::ProcessGame(FString Guess)
+{
+    if(HiddenWord == Guess)
+    {
+        PrintLine(TEXT("You have Won!"));
+        EndGame();
+        return;
+    }
+
+    if (Guess.Len()!=HiddenWord.Len())
+    {
+        PrintLine(TEXT("The HiddenWord has %i chars."),HiddenWord.Len());
+        PrintLine(TEXT("Sorry, try guessing again!\nYou %i lives remaining."),Lives);
+        return;
+    }
+
+    if(!IsIsogram(Guess))
+    {
+        PrintLine(TEXT("Type a Isogarm word please,try again!"));
+        return;
+    }
+
+    --Lives;
+    PrintLine(TEXT("lose a life!"));
+
+    if(Lives<=0)
+    {
+        ClearScreen();
+        PrintLine(TEXT("You have no lives left!"));
+        PrintLine(TEXT("The HiddenWord is %s."),*HiddenWord);
+        
+        EndGame();
+        return;
+    }
+
+    PrintLine(TEXT("Guess again,you have %i lives left!"),Lives);
+}
+
+bool UBullCowCartridge::IsIsogram(FString Word) const
+{
+    for (int32 i = 0; i < Word.Len()-1; ++i)
+    {
+        for (int32 j = i+1; j < Word.Len(); ++j)
+        {
+            if(Word[i]==Word[j])return false;
+        }
+    }
+    return true;
+}
+
+void UBullCowCartridge::DownloadWords()
+{
+    TSharedRef<IHttpRequest> Request = FHttpModule::Get().CreateRequest();
+    Request->SetURL("https://api.datamuse.com/words?ml=ringing+in+the+ears");
+    Request->SetVerb("GET");
+    Request->OnProcessRequestComplete().BindUObject(this,&UBullCowCartridge::OnDownloadComplete);
+    Request->ProcessRequest();
+}
+
+void UBullCowCartridge::OnDownloadComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess)
+{
+    if(bSuccess)
+    {
+        const FString RespJsonData = Response->GetContentAsString();
+      
+        FString JsonStr;  
+        TArray<TSharedPtr<FJsonValue>> JsonParsed;
+        const TSharedRef< TJsonReader<TCHAR> > JsonReader = TJsonReaderFactory<TCHAR>::Create(RespJsonData);
+
+        const bool bFlag = FJsonSerializer::Deserialize(JsonReader, JsonParsed);  
+        if (bFlag)  
+        {
+            const int32 ArrayLength = JsonParsed.Num();
+            PrintLine(TEXT("Data Lenght %i"),ArrayLength);
+            for(const TSharedPtr<FJsonValue> Value:JsonParsed)
+            {
+                FString FWord = Value->AsObject()->GetStringField("word");
+                int32   Score = Value->AsObject()->GetIntegerField("score");
+                if (GEngine)
+                {
+                    FColor LogColor;
+                    if(IsIsogram(FWord))
+                    {
+                        LogColor=FColor::Green;
+                        HiddenWords.Emplace(FWord);
+                    }else
+                    {
+                        LogColor=FColor::Red;
+                    }
+                    GEngine->AddOnScreenDebugMessage(-1, 5.f, LogColor, FWord);
+                }
+            }
+        }
+        
+        SetupGame();
+    }
 }
--- BullCowGame/Source/BullCowGame/BullCowCartridge.h
+++ BullCowGame/Source/BullCowGame/BullCowCartridge.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "IHttpRequest.h"
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
+
+const TArray<FString> Words = {TEXT("cakes"),TEXT("water"),TEXT("plums"),TEXT("kings"),TEXT("knife")};
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
@@ -15,7 +18,18 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
 
+	void SetupGame();
+	void EndGame();
+	void ProcessGame(FString Guess);
+	bool IsIsogram(FString Word) const;
+
+	void DownloadWords();
+	void OnDownloadComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bSuccess);
+
 	// Your declarations go below!
 	private:
-	
+	FString HiddenWord;
+	int32	Lives;
+	bool	bGameOver;
+	TArray<FString> HiddenWords;
 };
